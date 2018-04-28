db6579e9043dbf27f12a3169caa7872a5923518c
arataaoshima <newbluelight1031@gmail.com>
2018-04-28 23:28:11 +0000

    install bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,28 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find_by(id: params[:id])
+    end
+    
+    def index
+        @articles = Article.all
+        
+    end
+    
+    
+    def update
+        @article = Article.find_by(id: params[:id])
+      if @article.update(article_params)
+          flash[:notice] = "Post is sucessfully updated"
+          redirect_to article_path(@article)
+      else
+          render 'edit'
+      end
+    end
+    
+    
+    
     def create
         #render plain: params[:article].inspect
         #@article = Article.new(title: params[:title], description: params[:description])
@@ -20,6 +42,14 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def destroy
+        @article = Article.find_by(id:params[:id])
+        @article.destroy
+        flash[:notice] = "Post is sucessfully deleted"
+        redirect_to articles_path
+    end
+    
     
     private
     
