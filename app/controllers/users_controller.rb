0a58900848ef8387e3c9ad7ff55ea617fa0cc003
arataaoshima <newbluelight1031@gmail.com>
2018-05-02 01:37:28 +0000

    create show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,11 @@ class UsersController < ApplicationController
         
     end
     
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    
     def create
         @user = User.new(user_params)
     
